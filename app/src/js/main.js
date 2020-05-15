75600fd8fcbb687e0e84bc1d7926a00541bed114
Matheus Silva <matheushds.br@gmail.com>
2020-05-15 02:45:27 -0300

    feat(index): validate email

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,10 +1,19 @@
 $(function() {
-  $(".email").blur(function(event){
-    $(this).css({"background-color":"red", "color":"white"});
-    function validateEmail(email) {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(String(email).toLowerCase());
+  function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  //$(".email").on('blur', function() {
+  //  console.log(validateEmail($(this).val()));
+  //});
+  $("form").submit(function(e) {
+    e.preventDefault();
+    if (validateEmail($(this).find($(".email")).val())) {
+      $(this).find($(".email")).removeAttr("style");
+      console.log("Enviou..");
+    } else {
+      $(this).find($(".email")).css("border", "1px solid red");
+      console.log("Não enviou");
     }
-    console.log(validateEmail($(this).val()));
   });
 });
