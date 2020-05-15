bd54c8f61e94ad2331b2c1c389961a54666552fa
Matheus Silva <matheushds.br@gmail.com>
2020-05-15 17:47:38 -0300

    feat(index): add text error email and mask cpf

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,17 +3,19 @@ $(function() {
     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
-  //$(".email").on('blur', function() {
-  //  console.log(validateEmail($(this).val()));
-  //});
+
+  $('.cpf').mask('000.000.000-00');
+
   $("form").submit(function(e) {
     e.preventDefault();
     if (validateEmail($(this).find($(".email")).val())) {
-      $(this).find($(".email")).removeAttr("style");
-      console.log("Enviou..");
+     $(this).find($(".email")).removeAttr("style");
+     $(this).find($(".span-email")).hide();
     } else {
+      $(this).find($(".span-email")).show();
+      $(this).find($(".span-email")).text("*Email Inválido!");
+      $(this).find($(".span-email")).css("color", "red");
       $(this).find($(".email")).css("border", "1px solid red");
-      console.log("Não enviou");
     }
   });
 });
