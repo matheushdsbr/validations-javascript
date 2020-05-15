04c4bd733c38058a57a1315c8688ce648adb2a14
Matheus Silva <matheushds.br@gmail.com>
2020-05-15 01:52:01 -0300

    feat(index): add Email Validation

---
+++ app/src/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(".email").blur(function(event){
+    $(this).css({"background-color":"red", "color":"white"});
+    function validateEmail(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    }
+    console.log(validateEmail($(this).val()));
+  });
+});
